014694a58c57886dd09ef1de0f1dde9b6069c91e
hyunji <guswl1599@gmail.com>
2020-05-27 17:29:12 +0900

    step01,02

---
+++ Step02_DataType/src/test/main/MainClass04.java
@@ -0,0 +1,30 @@
+package test.main;
+/*
+ * [java 기본 데이터 type]
+ * 
+ * 2. 논리형(boolean)
+ * 
+ * -true,false 두가지 값중 하나이다.
+ * -true,false 를 직접 쓰거나 비교연산 혹은 논리 연산의 결과로 얻어낼수있다.
+ * -참과 거짓을 나타내는 데이터 type
+ */
+public class MainClass04 {
+	public static void main(String[] args) {
+		//논리형변수선언과 동시에 값 대입하기
+		boolean isRun=true;
+		if(isRun) {
+			System.out.println("달려요");
+		}
+		//비교연산의 결과로 얻어진 boolean 값 담기
+		boolean isGreater=10>1;
+		//논리연산의 결과로 얻어진 boolean 값 담기
+		boolean result=true||false;
+		
+		//한번 선언한 변수는 다시 선언할수없다.
+		//boolean result=false;
+		result=false; //변수에 다른 값을 대입할수있다.(동일한 type인 경우)
+		//다른 type 값은 변수에 담을수 없다.
+		//result=10;
+		
+	}
+}
